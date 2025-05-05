6458ea23a4355eb56657da05d7e267703cd7fe9e
sina <sina.tabriziyan@gmail.com>
2025-05-05 23:05:14 +0330

    ver 1.2.3

---
+++ SPView/src/main/java/com/sina/spview/extviews/fragments/lifeScopes.kt
@@ -0,0 +1,7 @@
+/**
+ * Created by ST on 5/5/2025.
+ * Author: Sina Tabriziyan
+ * @sina.tabriziyan@gmail.com
+ */
+package com.sina.spview.extviews.fragments
+
--- SPView/src/main/java/com/sina/spview/system/Vibrate.kt
+++ SPView/src/main/java/com/sina/spview/system/Vibrate.kt
@@ -1,5 +1,12 @@
 package com.sina.spview.system
 
+
+import android.content.Context
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import androidx.fragment.app.Fragment
+
 fun Fragment.vibrate(durationMillis: Long = 500) {
     val vibrator = requireContext().getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
