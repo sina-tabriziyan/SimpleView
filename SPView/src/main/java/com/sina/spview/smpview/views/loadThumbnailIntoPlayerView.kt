7a125f425c250f7fee3362f00e9e9566d4a32c9a
sina <sina.tabriziyan@gmail.com>
2025-05-19 15:59:42 +0330

    ver 1.5.3

--- SPView/src/main/java/com/sina/spview/smpview/views/loadThumbnailIntoPlayerView.kt
+++ SPView/src/main/java/com/sina/spview/smpview/views/loadThumbnailIntoPlayerView.kt
@@ -12,25 +12,11 @@ import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 
 fun loadThumbnailIntoPlayerView(playerView:PlayerView, videoUri:Uri) {
-    // Initialize ExoPlayer
     val player = ExoPlayer.Builder(playerView.context).build()
-
-    // Set the player to the PlayerView
     playerView.player = player
-
-    // Create a MediaItem with the video Uri
-    val mediaItem = MediaItem.fromUri(videoUri)
-
-    // Set the MediaItem to the player
-    player.setMediaItem(mediaItem)
-
-    // Prepare the player but don't start playing
+    player.setMediaItem( MediaItem.fromUri(videoUri))
     player.prepare()
-
-    // Optionally, you can seek to a specific position (e.g., 1 second) to display a different thumbnail
     player.seekTo(0) // Seek to the start (or any other time)
-
-    // Release the player when it's no longer needed (like when the view is detached or recycled)
     playerView.addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
         override fun onViewAttachedToWindow(v: View) {}
         override fun onViewDetachedFromWindow(v: View) {
