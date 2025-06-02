af37c11b4ae8c7cdc434446be3acf5f7a60efd95
sina <sina.tabriziyan@gmail.com>
2025-06-02 10:57:00 +0330

    ver 1.7.8

--- SPView/src/main/java/com/sina/spview/models/AudioState.kt
+++ SPView/src/main/java/com/sina/spview/models/AudioState.kt
@@ -5,6 +5,6 @@ sealed class AudioState {
     data class Playing(val position: Int, val progress: Int, val duration: Int) : AudioState()
     data class Paused(val position: Int, val progress: Int) : AudioState()
     data class Stopped(val position: Int) : AudioState()
-    data class Error(val itemTag: Int, val message: String): AudioState()
+    data class Error(val position: Int, val message: String): AudioState()
     data class AudioName(val fileName: String) : AudioState()
 }
