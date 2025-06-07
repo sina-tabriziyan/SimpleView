7ba624140bc165d13add6e2806a32352be5abf30
sina <sina.tabriziyan@gmail.com>
2025-06-07 16:39:54 +0330

    ver 1.9.8

--- SPView/src/main/java/com/sina/spview/donwload/ProgressInterceptor.kt
+++ SPView/src/main/java/com/sina/spview/donwload/ProgressInterceptor.kt
@@ -5,27 +5,26 @@
  */
 package com.sina.spview.donwload
 
-import com.sina.spview.states.ProgressResponseBody
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class ProgressInterceptor(
-    private var progressListener: ProgressListener,
-    private var downloadId: Int = 0
-) : Interceptor {
-
-    override fun intercept(chain: Interceptor.Chain): Response {
-        val originalResponse = chain.proceed(chain.request())
-
-        if (downloadId == 0)
-            return originalResponse
-
-        return originalResponse.newBuilder()
-            .body(originalResponse.body?.let {
-                ProgressResponseBody(
-                    chain.request().url.toUrl().toString(), it, progressListener
-                )
-            })
-            .build()
-    }
-}
+//class ProgressInterceptor(
+//    private var progressListener: ProgressListener,
+//    private var downloadId: Int = 0
+//) : Interceptor {
+//
+//    override fun intercept(chain: Interceptor.Chain): Response {
+//        val originalResponse = chain.proceed(chain.request())
+//
+//        if (downloadId == 0)
+//            return originalResponse
+//
+//        return originalResponse.newBuilder()
+//            .body(originalResponse.body?.let {
+//                ProgressResponseBody(
+//                    chain.request().url.toUrl().toString(), it, progressListener
+//                )
+//            })
+//            .build()
+//    }
+//}
